9966f243019d16b1ab4ef58e3dc312873c798285
hertzpaul <144059425+hertzpaul@users.noreply.github.com>
2024-02-06 15:58:43 +0100

    Add files via upload

---
+++ Lektion 1/Strings-regnr/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+Console.Write("Mata in registeringsnummer: ");
+string userInput = Console.ReadLine();
+        
+     if(IsValidInput(userInput))   
+     {
+        Console.WriteLine("JA, det är giltigt!");
+     } else
+     {
+        Console.WriteLine("Nej, inte giltigt :(");
+     }
+
+
+    static bool IsValidInput(string input)
+    {
+        Regex regex = new Regex(@"^[a-zA-Z]{3}\d{3}|[a-zA-Z]{3}\d{2}[a-zA-Z]$");
+
+        return regex.IsMatch(input);
+    }
+
+
