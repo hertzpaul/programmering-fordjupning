967b247af53b7e9c3d122e85243195acf4f2dac1
hertzpaul <144059425+hertzpaul@users.noreply.github.com>
2024-02-08 14:45:47 +0100

    Add files via upload

---
+++ Lektion 1/Strings-password/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Text.RegularExpressions;
+    {
+        bool isValid = false;
+
+        while (!isValid)
+        {
+            Console.Write("Testa ditt lösenord: ");
+            string password = Console.ReadLine();
+
+            if (IsValidPassword(password))
+            {
+                Console.WriteLine("True, det är giltigt!");
+                isValid = true;
+            }
+            else
+            {
+                Console.WriteLine("False, det är inte giltigt! Försök igen.");
+            }
+        }
+
+        Console.ReadLine();
+    }
+
+    static bool IsValidPassword(string input)
+    {
+        Regex regex = new Regex("^(?=.*?[A-Z]{2,})(?=.*?[a-z]{2,})(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
+        return regex.IsMatch(input);
+    }
+
+
+
